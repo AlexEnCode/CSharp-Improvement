0e15350f43dcbc50ef7563990ddb55f5a82e00cd
AlexEnCode <alexgabrielline@gmail.com>
2024-01-08 15:23:11 +0100

    feat

---
+++ Salaire2/Commercial.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exo_salaire
+{
+    internal class Commercial
+    {
+        private double _ca;
+        private double _commission;
+
+
+        public double Ca { get => _ca; set => _ca = value; }
+        public double Commission { get => _commission; set => _commission = value; }
+
+
+        public Commercial(double ca, double commission)
+        {
+            Ca = ca;
+            Commission = commission;
+        }
+
+    }
+}
---
+++ SalarieAvecHeritage/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Citerne
+{
+
+    internal class WaterTank
+    {
+
+        // Atributs 
+        private string _name = "Chateau d'Eau";
+        private int _poids = 10;
+        private int _capacite = 10;
+        private int _niveau = 0;
